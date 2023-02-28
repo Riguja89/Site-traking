08a1dfb9f0ff2aa85ffe865fb84f8116973e46b0
Riguja89 <jaimegutierrios@gmail.com>
2023-02-28 08:56:41 -0500

    fixed credentials exposed

--- Api/src/db.js
+++ Api/src/db.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
+const {DB_USER,DB_PASSWORD}=process.env;
+
 
 //"mongodb+srv://<username>:<password>@cluster0.ixo4j.mongodb.net/?retryWrites=true&w=majority
 
-mongoose.connect("mongodb+srv://user_sitetrack:o143gZKwWYXiuEvQ@cluster0.ixo4j.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ixo4j.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
     console.log("conectado a la db")
 })
 .catch(error=> console.error(error))
